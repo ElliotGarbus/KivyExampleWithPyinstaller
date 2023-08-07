d1e67846e56e26598554fe9369a0c463b66e59d5
ElliotGarbus <elliotg2@cox.net>
2023-08-07 15:14:56 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+os.environ['KIVY_NO_CONSOLELOG'] = '1'
+
+from kivy.app import App
+from kivy.uix.label import Label
+import itertools
+
+
+class HelloLabel(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hellos = itertools.cycle(['Hello to you!', 'Hey There!', "What's up?"])
+
+    def say_hello(self):
+        self.text = next(self.hellos)
+
+
+class ExampleWithPyinstallerApp(App):
+    pass
+
+
+ExampleWithPyinstallerApp().run()
